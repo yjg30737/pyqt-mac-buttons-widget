625e51aa94dce6eff58364f215e6bcecd44bae81
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-12 13:46:54 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pyqt-mac-min-max-close-buttons-widget',
+    name='pyqt-mac-buttons-widget',
     version='0.1.0',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
-    description='PyQt Mac min/max/close buttons widget',
-    url='https://github.com/yjg30737/pyqt-mac-min-max-close-buttons-widget.git',
+    description='PyQt macOS style of titlebar buttons (e.g. min/max/close) widget',
+    url='https://github.com/yjg30737/pyqt-mac-buttons-widget.git',
     install_requires=[
         'PyQt5>=5.8',
         'pyqt-titlebar-buttons-widget>=0.0.1'
