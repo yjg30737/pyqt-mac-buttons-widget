cec12dbde0bbcf223c6dcd6ba4ca752963cc3d36
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-12 13:45:13 +0900

    Update package's url which was recently uploaded to pypi

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(
     url='https://github.com/yjg30737/pyqt-mac-min-max-close-buttons-widget.git',
     install_requires=[
         'PyQt5>=5.8',
-        'pyqt-titlebar-buttons-widget @ git+https://git@github.com/yjg30737/pyqt-titlebar-buttons-widget.git@main'
+        'pyqt-titlebar-buttons-widget>=0.0.1'
     ]
 )
