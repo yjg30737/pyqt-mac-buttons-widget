5cbc09d86fda25a230c5ef3a5d92c560ef9f7d98
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-23 18:26:23 +0900

    Override function of parent class

--- pyqt_mac_buttons_widget/macButtonsWidget.py
+++ pyqt_mac_buttons_widget/macButtonsWidget.py
@@ -1,15 +1,16 @@
 from PyQt5.QtGui import QColor
+from PyQt5.QtWidgets import qApp
 from pyqt_titlebar_buttons_widget import TitlebarButtonsWidget
 
 
 class MacButtonsWidget(TitlebarButtonsWidget):
     def __init__(self, base_widget=None, hint=['min', 'max', 'close']):
         super().__init__(base_widget, hint)
+        self.__size = qApp.font().pointSize()
         self.__initVal()
         self.__initUi()
 
     def __initVal(self):
-        self.__size = 17
         self.__border_width = self.__size // 20
         self.__border_radius = self.__size // 2
         self.__macBtnStyle = ''
@@ -41,3 +42,13 @@ class MacButtonsWidget(TitlebarButtonsWidget):
                              '''
 
         btn.setStyleSheet(self.__macBtnStyle)
+
+    def setButtonSize(self, w, h):
+        self.__size = w
+        self.__initVal()
+        btns = [self._closeBtn, self._minimizeBtn, self._maximizeBtn]
+        colors = ['#DD0000', '#AA8800', '#008800']
+        for i in range(len(btns)):
+            btn = btns[i]
+            btn.setFixedSize(self.__size, self.__size)
+            self.__setStyleForEachButton(btn, colors[i])
